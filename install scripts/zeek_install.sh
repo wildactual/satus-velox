912f3b70a8813db400ff96280950eedffbfcd7bf
deathnmind <mike.burton@gmail.com>
2021-09-29 19:05:21 -0400

    more updates

---
+++ install scripts/zeek_install.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+sudo apt update
+sudo apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python3 python3-dev swig zlib1g-dev
+git clone --recursive https://github.com/zeek/zeek
+cd zeek
+./configure
+make
+sudo make install
+sudo ln --symbolic /usr/local/zeek/bin/zeek /usr/bin/zeek
+# for zkg
+sudo apt install python3-pip
+sudo pip3 install GitPython semantic-version
+sudo ln --symbolic /usr/local/zeek/bin/zkg /usr/bin/zkg
+
+#run zeek and read test.pcap
+zeek /usr/local/zeek/share/zeek/site/local.zeek -r test.pcap
