9df19111865922f5fb2305b0e54c67985d32075f
deathnmind <mike.burton@gmail.com>
2021-11-04 18:30:22 -0400

    vscode extensions updated

--- scripts/install scripts/new_desktop.sh
+++ scripts/install scripts/new_desktop.sh
@@ -39,4 +39,4 @@ sudo apt autoremove -yqq && sudo apt autoclean -yqq
 # added python-pip packages
 pip install stormssh
 # install vscode extensions
-for extensions in hookyqr.beautify wheredoesyourmindgo.gruvbox-concoctis shd101wyy.markdown-preview-enhanced davidanson.vscode-markdownlint robertrossmann.remedy ms-vscode-remote.remote-ssh ms-vscode-remote.remote-ssh-edit ms-vscode-remote.remote-wsl ms-python.vscode-pylance ms-python.python github.vscode-pull-request-github eamodio.gitlens phplasma.csv-to-table; do code --install-extension $extensions; done;
+for extensions in hookyqr.beautify streetsidesoftware.code-spell-checker wheredoesyourmindgo.gruvbox-concoctis davidanson.vscode-markdownlint robertrossmann.remedy ms-vscode-remote.remote-ssh ms-vscode-remote.remote-ssh-edit ms-vscode-remote.remote-wsl ms-python.vscode-pylance ms-python.python github.vscode-pull-request-github eamodio.gitlens phplasma.csv-to-table yzhang.markdown-all-in-one mechatroner.rainbow-csv splunk.splunk visualstudioexptteam..vscodeintellicode redhat.vscode-yaml; do code --install-extension $extensions; done;
